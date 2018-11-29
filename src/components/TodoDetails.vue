22621d3fb2ab189e1c7a820e6fcec43221c0f4ed
Soul Evans <soulevans07@gmail.com>
2018-11-29 01:12:01 +0100

    added assignee and due date field for details view

--- src/components/TodoDetails.vue
+++ src/components/TodoDetails.vue
@@ -8,11 +8,19 @@
                 Mark Complete
             </div>
         </div>
-        <div class="detail-content">
-
-            <div class="todo-detail-content">
-                <input type="text" class="todo-detail-title"
-                       v-model="todo.name"/>
+        <div class="todo-detail-content">
+            <input type="text" class="todo-detail-title"
+                   placeholder="Write a task name"
+                   v-model="todo.name"/>
+            <div class="assignee-due-date-row">
+                <div class="assignee">
+                    <div class="assignee-img"></div>
+                    <div class="assignee-label">
+                        <div class="assigned-to">Assigned To</div>
+                        <div class="assignee-name">{{todo.assignee ? todo.assignee.username : 'Unassigned'}}</div>
+                    </div>
+                </div>
+                <div class="due_date">{{todo.due_date ? todo.due_date : 'Due Date'}}</div>
             </div>
         </div>
     </div>
@@ -25,6 +33,62 @@
 </script>
 
 <style>
+    .assignee-due-date-row {
+        flex: 1;
+        flex-direction: row;
+        padding: 10px 0;
+    }
+
+    .assignee {
+        float: left;
+        display: inline-flex;
+        border-radius: 30px;
+        border: 0;
+        min-width: 100px;
+        padding: 3px 15px 3px 3px;
+        flex-direction: row;
+        text-overflow: ellipsis;
+        /*margin-right: 80px;*/
+    }
+
+    .assignee:hover {
+        border: solid 1px #b7bfc6;
+        padding: 2px 15px 2px 2px;
+    }
+
+    .assignee-label {
+        flex-direction: column;
+    }
+
+    .assignee-img {
+        border-radius: 30px;
+        border: solid 1px #b7bfc6;
+        height: 33px;
+        width: 33px;
+        margin-right: 10px;
+    }
+
+    .assigned-to {
+        text-align: left;
+        line-height: normal;
+        font-size: 12px;
+        color: #848f99;
+    }
+
+    .assignee-name {
+        text-align: left;
+        line-height: normal;
+    }
+
+    .due_date {
+        display: inline-flex;
+        border-radius: 30px;
+        border: solid 1px #b7bfc6;
+        min-width: 100px;
+        padding: 5px 15px;
+        line-height: 30px;
+    }
+
     .mark-btn {
         float: left;
         background: #fff;
@@ -57,7 +121,7 @@
         fill: #222b37;
     }
 
-    .mark-btn:active{
+    .mark-btn:active {
         background-color: #edf1f2;
         box-shadow: none;
     }
@@ -70,11 +134,24 @@
     }
 
     .todo-detail-content {
-        padding: 0 25px;
+        display: flex;
+        flex-direction: column;
+        flex: 1;
+        padding: 10px 25px 0;
     }
 
     .todo-detail-title {
+        vertical-align: center;
         font-size: 30px;
+        padding: 1px 15px;
+        outline: 0;
+        border: 0;
+    }
+
+    .todo-detail-title:hover {
+        border: solid 1px #d5dce0;
+        border-radius: 4px;
+        padding: 0 14px;
     }
 
     .detail-box {
